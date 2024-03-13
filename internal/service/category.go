e90dbe6e2ab5946979cafc116746e71abd5d42ba
Pedro Sousa <36779714+SousaPedro11@users.noreply.github.com>
2024-03-12 22:22:38 -0300

    feat: add category service

---
+++ internal/service/category.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/sousapedro11/fc-grpc-go/internal/database"
+	"github.com/sousapedro11/fc-grpc-go/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}, nil
+}
